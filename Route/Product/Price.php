deb79a496035de8390fe40f8a6db65b4238c12bc
TomYoYo <tom.pueyo@gmail.com>
2017-12-19 12:46:20 +0100

    remove price

--- Route/Product/Price.php
+++ Route/Product/Price.php
@@ -70,4 +70,13 @@ class Price
             $url = $this->url;
         return $this->master->patch($url, $price->getIdPrice(), $price, $this->entity, $format);
     }
+
+    public function remove(\monsieurgourmand\Bundle\InterfaceBundle\Model\Price $price, Supplier $supplier = null)
+    {
+        if ($supplier)
+            $url = "/suppliers/" . $supplier->getIdSupplier() . $this->url;
+        else
+            $url = $this->url;
+        return $this->master->remove($url, $price->getIdPrice());
+    }
 }
