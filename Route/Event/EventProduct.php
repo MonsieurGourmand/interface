e6f4e1e2dc970a01df96b72e5f1b3424f64a1876
tom <tom.pueyo@gmail.com>
2018-03-13 17:26:43 +0100

    eventProducts params

--- Route/Event/EventProduct.php
+++ Route/Event/EventProduct.php
@@ -25,9 +25,8 @@ class EventProduct
         $this->url = "/operations/" . $event->getOperation()->getIdOperation() . "/events/" . $event->getIdEvent() . "/products";
     }
 
-    public function getAll($format = MGD::FORMAT_OBJECT)
+    public function getAll($format = MGD::FORMAT_OBJECT, $params = array())
     {
-        $params = array();
         return $this->master->getAll($this->url, $this->entity, $params, $format);
     }
 
