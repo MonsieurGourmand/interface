4826f9092b91818ed92a0429be1580425bafd273
TomYoYo <tom.pueyo@gmail.com>
2019-01-25 12:41:36 +0100

    get Direct Amounts

---
+++ Route/Amount.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: remi
+ * Date: 1/20/17
+ * Time: 6:10 PM
+ */
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route;
+
+
+use monsieurgourmand\Bundle\InterfaceBundle\Model\Event;
+use monsieurgourmand\Bundle\InterfaceBundle\Service\MGD;
+
+class Amount
+{
+    private $entity;
+    private $url;
+    private $master;
+
+    public function __construct(MGD $master)
+    {
+        $this->master = $master;
+        $this->entity = \monsieurgourmand\Bundle\InterfaceBundle\Model\Amount::class;
+        $this->url = "/amounts";
+    }
+
+    public function getAll($format = MGD::FORMAT_OBJECT, $params = array())
+    {
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
+    }
+
+}
