f457c0bc734da21ecc38f63592c672c1e96b0e3d
LiliceD <lilice.dhn@gmail.com>
2019-05-09 15:30:23 +0200

    Test fix picture routes

---
+++ Route/Supplier/Product/Picture.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route\Supplier\Product;
+
+
+/**
+ * Class Picture
+ *
+ * @author Alice Dahan <lilice.dhn@gmail.com>
+ */
+class Picture extends \monsieurgourmand\Bundle\InterfaceBundle\Route\Product\Picture
+{
+
+}
