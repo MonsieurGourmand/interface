1327132d2f1f9421667487fda843c0861359161c
LiliceD <lilice.dhn@gmail.com>
2020-01-29 09:49:54 +0100

    Add type to Shop

---
+++ Model/ShopType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Model;
+
+/**
+ * Class ShopType
+ */
+class ShopType
+{
+    /** @var int */
+    private $id;
+
+    /** @var string */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return ShopType
+     */
+    public function setId(int $id): ShopType
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return ShopType
+     */
+    public function setName(string $name): ShopType
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
