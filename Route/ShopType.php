499c593ca810217c5f6c92e34e9668e2471e3c4f
LiliceD <lilice.dhn@gmail.com>
2020-01-30 15:52:02 +0100

    Add getAll ShopType route

---
+++ Route/ShopType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route;
+
+use monsieurgourmand\Bundle\InterfaceBundle\Service\MGD;
+
+class ShopType
+{
+    /** @var MGD */
+    private $master;
+
+    /** @var string */
+    private $entity;
+
+    /** @var string */
+    private $url;
+
+    /**
+     * ShopType constructor.
+     *
+     * @param MGD $master
+     */
+    public function __construct(MGD $master)
+    {
+        $this->master = $master;
+        $this->entity = ShopType::class;
+        $this->url = '/shoptypes';
+    }
+
+    /**
+     * @param array $params
+     * @param string $format
+     * @return ShopType[]
+     */
+    public function getAll($params = [], $format = MGD::FORMAT_OBJECT): array
+    {
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
+    }
+}
