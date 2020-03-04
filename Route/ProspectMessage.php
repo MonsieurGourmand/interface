866bb1144cd73e2762a97ffa1ac63665bac57b57
LiliceD <lilice.dhn@gmail.com>
2020-03-04 15:59:57 +0100

    Update prospect routes

---
+++ Route/ProspectMessage.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: remi
+ * Date: 1/20/17
+ * Time: 6:10 PM
+ */
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route;
+
+use monsieurgourmand\Bundle\InterfaceBundle\Model\Prospect;
+use monsieurgourmand\Bundle\InterfaceBundle\Service\MGD;
+
+class ProspectMessage
+{
+    private $entity;
+    private $url;
+    private $master;
+
+    public function __construct(Prospect $prospect)
+    {
+        $this->master = $prospect->getMaster();
+        $this->entity = \monsieurgourmand\Bundle\InterfaceBundle\Model\ProspectMessage::class;
+        $this->url = "/prospectsmessages";
+    }
+
+    public function getAll($params = [], $format=MGD::FORMAT_OBJECT)
+    {
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
+    }
+
+    public function get(int $id, $params = [], $format=MGD::FORMAT_OBJECT)
+    {
+        return $this->master->get($this->url, $id, $this->entity, $format, $params);
+    }
+
+    public function post(\monsieurgourmand\Bundle\InterfaceBundle\Model\ProspectMessage $message, $format=MGD::FORMAT_OBJECT)
+    {
+        return $this->master->post($this->url, $message, $this->entity, $format);
+    }
+
+    public function put(\monsieurgourmand\Bundle\InterfaceBundle\Model\ProspectMessage $message, $format=MGD::FORMAT_OBJECT)
+    {
+        return $this->master->put($this->url, $message->getIdMessage(), $message, $this->entity, $format);
+    }
+
+    public function remove(\monsieurgourmand\Bundle\InterfaceBundle\Model\ProspectMessage $message)
+    {
+        return $this->master->remove($this->url, $message->getIdMessage());
+    }
+}
