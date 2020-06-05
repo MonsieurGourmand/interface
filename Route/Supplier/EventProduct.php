a1af046cd1903dbbf1aca7bd7137fb8aefde3708
TomYoYo <tom.pueyo@gmail.com>
2020-06-05 20:13:01 +0200

    supplierEventProducts endpoint

---
+++ Route/Supplier/EventProduct.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route\Supplier;
+
+use monsieurgourmand\Bundle\InterfaceBundle\Model\Supplier;
+use monsieurgourmand\Bundle\InterfaceBundle\Service\MGD;
+use monsieurgourmand\Bundle\InterfaceBundle\Model\EventProduct as EventProductModel;
+
+class EventProduct
+{
+    /** @var string */
+    private $entity;
+    /** @var string */
+    private $url;
+    /** @var MGD */
+    private $master;
+
+    /**
+     * EventProduct constructor.
+     * @param Supplier $supplier
+     */
+    public function __construct(Supplier $supplier)
+    {
+        $this->master = $supplier->getMaster();
+        $this->entity = EventProductModel::class;
+        $this->url = "/suppliers/" . $supplier->getIdSupplier() . "/eventproducts/";
+    }
+
+    /**
+     * @param string $format
+     * @param array $params
+     * @return EventProductModel[]
+     */
+    public function getAll($params = array(), $format = MGD::FORMAT_OBJECT)
+    {
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
+    }
+}
