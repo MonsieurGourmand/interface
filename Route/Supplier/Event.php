2560a5007b4bacdcfb9761e07d1d4d124d1d6014
LiliceD <lilice.dhn@gmail.com>
2020-06-10 14:48:59 +0200

    Add SupplierEvent route

---
+++ Route/Supplier/Event.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace monsieurgourmand\Bundle\InterfaceBundle\Route\Supplier;
+
+use monsieurgourmand\Bundle\InterfaceBundle\Model\Supplier;
+use monsieurgourmand\Bundle\InterfaceBundle\Service\MGD;
+use monsieurgourmand\Bundle\InterfaceBundle\Model\Event as EventModel;
+
+class Event
+{
+    /** @var string */
+    private $entity;
+    /** @var string */
+    private $url;
+    /** @var MGD */
+    private $master;
+
+    /**
+     * Event constructor.
+     * @param Supplier $supplier
+     */
+    public function __construct(Supplier $supplier)
+    {
+        $this->master = $supplier->getMaster();
+        $this->entity = EventModel::class;
+        $this->url = "/suppliers/" . $supplier->getIdSupplier() . "/events";
+    }
+
+    /**
+     * @param string $format
+     * @param array $params
+     * @return EventModel[]
+     */
+    public function getAll($params = array(), $format = MGD::FORMAT_OBJECT)
+    {
+        return $this->master->getAll($this->url, $this->entity, $params, $format);
+    }
+}
