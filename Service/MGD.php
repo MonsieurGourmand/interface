35f530175ed0cbe70a33cd109b9fc96dcf15b73f
LiliceD <lilice.dhn@gmail.com>
2020-09-11 11:20:36 +0200

    Add Sponsorship model and route

--- Service/MGD.php
+++ Service/MGD.php
@@ -4,6 +4,7 @@ namespace monsieurgourmand\Bundle\InterfaceBundle\Service;
 
 use monsieurgourmand\Bundle\InterfaceBundle\Route\BusinessLine;
 use monsieurgourmand\Bundle\InterfaceBundle\Route\ProductCertification;
+use monsieurgourmand\Bundle\InterfaceBundle\Route\Sponsorship;
 use monsieurgourmand\Bundle\InterfaceBundle\Route\SupplierCertification;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use monsieurgourmand\Bundle\InterfaceBundle\Route\Action;
@@ -114,6 +115,7 @@ class MGD
     public $supplierCertification;
     public $productCertification;
     public $businessLines;
+    public $sponsorship;
 
     public function __construct(Session $session = null, Parser $parser, Serializer $serializer, $client_id, $client_secret, $callback, $oauthRoot)
     {
@@ -169,6 +171,7 @@ class MGD
         $this->supplierCertification = new SupplierCertification($this);
         $this->productCertification = new ProductCertification($this);
         $this->businessLines = new BusinessLine($this);
+        $this->sponsorship = new Sponsorship($this);
     }
 
     public function login()
